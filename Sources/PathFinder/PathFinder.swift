2baa9b352f127a5752bbc90407ebed1f4b9dab90
Alon <alon02st@gmail.com>
2022-10-02 16:00:51 +0300

    Implemented PathFinder.getShortestPath, Added tests

--- Sources/PathFinder/PathFinder.swift
+++ Sources/PathFinder/PathFinder.swift
@@ -6,7 +6,42 @@ public struct PathFinder {
     }
     
     public func getShortestPath(from start: String, to destination: String) -> [String] {
-        // TODO: implement function
+        // Check edge cases
+        if nodes[start] == nil || nodes[destination] == nil { return [] }
+        if start == destination { return [start] }
+        
+        // Set unvisited nodes set, current path cost and current path variables
+        var unvisitedNodes: [String] = Array(self.nodes.keys)
+        var currentPathCost: [String: UInt32] = [start: 0]  // Node: Current Cost
+        var currentPath: [String: [String]] = [start: [start]]  // Node: Current Path
+        
+        // Get start node
+        guard let index = unvisitedNodes.firstIndex(of: start) else { return [] }
+        var currentNode = unvisitedNodes.remove(at: index)
+        
+        while !unvisitedNodes.isEmpty {
+            // Update neighbors paths cost
+            self.nodes[currentNode]?.forEach { neighbor, cost in
+                if unvisitedNodes.contains(neighbor) {
+                    if currentPathCost[currentNode]! + cost < currentPathCost[neighbor] ?? UInt32.max {
+                        currentPathCost[neighbor] = currentPathCost[currentNode]! + cost
+                        currentPath[neighbor] = currentPath[currentNode]
+                        currentPath[neighbor]!.append(neighbor)
+                    }
+                }
+            }
+            
+            // Get next node and remove it from unvisited nodes
+            // If there is no unvisited node with current path, set currentNode
+            currentNode = currentPathCost.sorted { $0.value < $1.value }.first {
+                unvisitedNodes.contains($0.key) }?.key ?? destination
+            if currentPathCost[currentNode] == UInt32.max || currentNode == destination {
+                return currentPath[destination] ?? []
+            }
+            guard let index = unvisitedNodes.firstIndex(of: currentNode) else { return [] }
+            unvisitedNodes.remove(at: index)
+        }
+        
         return []
     }
 }
--- Tests/PathFinderTests/PathFinderTests.swift
+++ Tests/PathFinderTests/PathFinderTests.swift
@@ -2,4 +2,35 @@ import XCTest
 @testable import PathFinder
 
 final class PathFinderTests: XCTestCase {
+    var simpleGraph: [String: [String: UInt32]] = [
+        "A": ["B": 2, "C": 6],
+        "B": ["A": 2, "D": 5],
+        "C": ["A": 6, "D": 3],
+        "D": ["B": 5, "C": 3, "E": 10, "F": 13],
+        "E": ["D": 10, "F": 6, "G": 2],
+        "F": ["D": 13, "E": 6, "G": 6],
+        "G": ["E": 2, "F": 6]
+    ]
+    
+    func testUnknownStartOrDestination() {
+        let finder = PathFinder(nodes: simpleGraph)
+        XCTAssertEqual(finder.getShortestPath(from: "0", to: "A"), [])
+        XCTAssertEqual(finder.getShortestPath(from: "A", to: "0"), [])
+    }
+    
+    func testPathToSameNode() {
+        let finder = PathFinder(nodes: simpleGraph)
+        XCTAssertEqual(finder.getShortestPath(from: "A", to: "A"), ["A"])
+    }
+    
+    func testSimpleGraph() {
+        let finder = PathFinder(nodes: simpleGraph)
+        XCTAssertEqual(finder.getShortestPath(from: "A", to: "G"), ["A", "B", "D", "E", "G"])
+    }
+    
+    func testNoConnection() {
+        simpleGraph["H"] = [:]
+        let finder = PathFinder(nodes: simpleGraph)
+        XCTAssertEqual(finder.getShortestPath(from: "A", to: "H"), [])
+    }
 }
