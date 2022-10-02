5a8a88e37b73b8348dcb2d8b5ca3a7b18cdb0ea0
Alon <alon02st@gmail.com>
2022-10-02 14:23:24 +0300

    Added PathFinder.getShortestPath decleration

--- Sources/PathFinder/PathFinder.swift
+++ Sources/PathFinder/PathFinder.swift
@@ -1,2 +1,12 @@
 public struct PathFinder {
+    var nodes: [String: [String: UInt32]]  // Node: [Node: Cost]
+    
+    init(nodes: [String: [String: UInt32]]) {
+        self.nodes = nodes
+    }
+    
+    public func getShortestPath(from start: String, to destination: String) -> [String] {
+        // TODO: implement function
+        return []
+    }
 }
